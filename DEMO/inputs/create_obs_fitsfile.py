b21517c01d1936d9e8bc88a240942f5e30bf04fd
Palma-Bifani <ppalma@das.uchile.cl>
2023-03-07 16:12:05 +0100

    cleaning

--- DEMO/inputs/create_obs_fitsfile.py
+++ DEMO/inputs/create_obs_fitsfile.py
@@ -3,11 +3,10 @@ from astropy.table import Table
 
 def creat_fitsfile(name_file):
     '''
-    Works from old format (txt file with: wave, flux, flux_err, flag, type, resolution, instrumen) to new fits format
+    Easy function to save .fits from dataframe 
+    Columns must be: wave [µm], fluxv[W m-2 µm-1], flux_errv[W m-2 µm-1], resolution, instrumen_name
     '''
     fileop = pd.read_csv(name_file+'.txt', header=None, sep='\s+')
-    fileop.columns=['WAV', 'FLX', 'ERR', 'a', 'b', 'RES', 'INS']
-    fileop = fileop.drop(['a', 'b'], axis=1)
-
+    fileop.columns=['WAV', 'FLX', 'ERR', 'RES', 'INS']
     t = Table.from_pandas(fileop)
     t.write(name_file+'.fits')
