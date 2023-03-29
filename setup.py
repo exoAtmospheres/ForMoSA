cc923d6272ce5f17884142b6db0a1c04ca5a0627
Palma-Bifani <ppalma@das.uchile.cl>
2023-03-29 05:47:19 +0200

    building doc

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requires():
     return reqs
 
 setup(name='ForMoSA',
-      version='1.0.7',
+      version='1.0.8',
       description='ForMoSA: Forward Modeling Tool for Spectral Analysis',
       url='https://github.com/exoAtmospheres/ForMoSA',
       author='P. Palma-Bifani, S. Petrus',
