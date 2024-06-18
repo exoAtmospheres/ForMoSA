a8f962f23b8f3834f38916fe6ab1cd6f1dc70238
DenisAll <33734583+DenisAll@users.noreply.github.com>
2024-06-18 15:48:38 +0200

    AD : Small change to include systemtics in plotting function for Hi resolution data

--- ForMoSA/plotting/plotting_class.py
+++ ForMoSA/plotting/plotting_class.py
@@ -688,8 +688,10 @@ class PlottingForMoSA():
                 # so the model is the sum of the planet model + the estimated stellar contribution
                 spectra = list(spectra) # Transform spectra to a list so that we can modify its values
                 spectra[indobs] = list(spectra[indobs])
-                model, planet_contribution, stellar_contribution, star_flx = spectra[indobs][3], spectra[indobs][9], spectra[indobs][10], spectra[indobs][11]
+                model, planet_contribution, stellar_contribution, star_flx, systematics = spectra[indobs][3], spectra[indobs][9], spectra[indobs][10], spectra[indobs][11], spectra[indobs][12]
                 spectra[indobs][3] = planet_contribution * model + stellar_contribution * star_flx
+                if len(systematics) > 0:
+                    spectra[indobs[3]] += systematics
 
             if len(spectra[indobs][0]) != 0:
                 if uncert=='yes':
