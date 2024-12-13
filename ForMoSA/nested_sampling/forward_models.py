b216b57c61f9dede3aaccf03b1df8ee4cea3b38d
Allan Denis <allan.denis@lam.fr>
2024-12-13 18:00:07 +0100

    AD : correcting typo

--- ForMoSA/nested_sampling/forward_models.py
+++ ForMoSA/nested_sampling/forward_models.py
@@ -1,7 +1,6 @@
 import numpy as np
 import scipy.optimize as optimize
 from ForMoSA.adapt.extraction_functions import continuum_estimate
-import ForMoSA.utils as utils
 
 
 def forward_model(global_params, wav_mod_spectro, res_mod_spectro, flx_cont_obs, flx_mod, star_flx_obs, star_flx_cont_obs, err_obs, transm_obs, flx_obs, system_obs, indobs):
