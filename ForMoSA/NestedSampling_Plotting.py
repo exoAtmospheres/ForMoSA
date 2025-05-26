d8a020bc8fc02d22f137276f05f3945c5911378e
adenis <adenis@lam.fr>
2025-05-26 13:11:06 +0200

    Factorising best_fit plotting function

--- ForMoSA/NestedSampling_Plotting.py
+++ ForMoSA/NestedSampling_Plotting.py
@@ -58,7 +58,90 @@ class NestedSampling_Plotting(object):
     @property
     def size(self):                    # Size used for the best fit plotting
         return self.plotting_config['size']
+    
+    
+    ##################################################
+    # Methods
+    ##################################################
         
+    
+    @staticmethod
+    def _plot_data_point(ax: matplotlib.axes.Axes, axr: matplotlib.axes.Axes, obs_wav: np.ndarray, obs_flx: np.ndarray, mod_flx: np.ndarray, std_global: float, color: str, edgecolor: str, marker: str, size, label: str=None, yerr: np.ndarray=None, xerr: np.ndarray=None, plot_model: str='plot') -> None:
+        '''
+        Plot a single data point (either spectroscopic or photometric) with optional error bars and model curve.
+    
+        Parameters:
+        ax    (matplotlib.axes.Axes): Main axes for the plot
+        axr   (matplotlib.axes.Axes): Residuals axes for the plot
+        obs_wav              (array): Observed wavelengths
+        obs_flx              (array): Observed flux
+        mod_flx              (array): Model flux
+        std_global           (float): Global standard deviation for residuals normalization
+        color                  (str): Color of the data points
+        edgecolor              (str): Edge color of the data points
+        marker                 (str): Marker style for the data points
+        size                 (float): Size of the markers
+        label                  (str): Label for the data points
+        err                  (array): Error values for the data
+        plot_model             (str): Type of plot for the model ('plot' or 'scatter')
+        
+        Authors: Allan Denis
+        '''
+        
+        # Plot the observed data with error bars
+        if marker == 'NA':
+            ax.plot(obs_wav, obs_flx, c=color, label=label)
+            axr.plot(obs_wav, (obs_flx - mod_flx) / std_global, c=color, alpha=0.7)
+        else:
+            sc = ax.scatter(obs_wav, obs_flx, c=color, edgecolors=edgecolor, marker=marker, s=size, label=label, linewidths=2)
+            axr.scatter(obs_wav, (obs_flx - mod_flx) / std_global, c=color, edgecolors=edgecolor, marker=marker, s=size, alpha=0.7, linewidths=2)
+    
+        # Add error bars if applicable
+        if yerr is not None:
+            ax.errorbar(obs_wav, obs_flx, yerr=yerr, fmt='none', ecolor=edgecolor, alpha=0.8)
+        if xerr is not None:
+            ax.errorbar(obs_wav, obs_flx, xerr=xerr, fmt='None', ecolor=edgecolor, alpha=0.8)
+    
+        # Optionally plot the model flux
+        if plot_model == 'plot':
+            ax.plot(obs_wav, mod_flx, c='black')
+        elif plot_model == 'scatter':
+            ax.scatter(obs_wav, mod_flx, marker='o', c='black', s=20)
+    
+        # Add residuals horizontal line at y=0
+        axr.axhline(0, color='k', linestyle='--', alpha=0.5)
+    
+        return sc  # Return scatter object for legend handling
+
+
+    @staticmethod
+    def _get_label(ins: str, label_type: str, used_labels: set, default_label: str) -> str:
+        '''
+        Determine the label for the data based on the instrument and whether label display is enabled.
+    
+        Parameters:
+        -----------
+            ins            (str): Instrument name
+            label_type     (str): Flag to determine if the instrument label should be shown
+            used_labels    (set): Set of already used labels to avoid duplicates
+            default_label  (str): Default label text
+    
+        Returns:
+            - label (str): The appropriate label to use.
+        
+        Authors: Allan Denis
+        '''
+        
+        if label_type == 'no':
+            label = default_label if default_label not in used_labels else None
+        else:
+            label = f"{ins}"
+        
+        if label:
+            used_labels.add(label)
+        
+        return label
+    
 
     def _plot_corner(self, results: dict, param_names: list, levels_sig: list=[0.997, 0.95, 0.68], bins: int=100, quantiles: tuple=(0.16, 0.5, 0.84), figsize: tuple=(15, 15)) -> matplotlib.figure.Figure:
         '''
@@ -74,7 +157,7 @@ class NestedSampling_Plotting(object):
         figsize      (tuple): Size of the figure to plot
 
         Returns:
-            fig (matplotlib.figure.Figure): Matplotlib Figure object
+            - fig (matplotlib.figure.Figure): Matplotlib Figure object
 
         '''
         
@@ -275,32 +358,72 @@ class NestedSampling_Plotting(object):
         return fig, ax
 
 
-    def _plot_fit(self, modif_data: dict, best_model: dict, figsize=(10, 7), uncert='yes', trans='yes', logx='no', logy='no', norm='no', label_ins='no') -> tuple[matplotlib.figure.Figure, matplotlib.axes._axes.Axes, matplotlib.axes._axes.Axes, matplotlib.axes._axes.Axes]:
+    def _get_plot_style(self, indobs: int, default_color: str='magenta', default_edge: str='darkmagenta', default_size: float=50, default_marker: str='o') -> tuple[str, str, str, float]:
+        '''
+        Method to extract the plotting style (color, edgecolor, marker, size) for the given observation index.
+    
+        Parameters:
+        ----------
+        indobs          (int): Index of the observation.
+        default_color   (str): Default color if the value is 'NA'.
+        default_edge    (str): Default edge color if the value is 'NA'.
+        default_size    float): Default marker size if the value is 'NA'.
+        default_marker  (str): Default marker type if the value is 'NA'.
+    
+        Returns:
+            - tuple: (color, edgecolor, marker, size)
+            
+        Authors: Allan Denis
+        '''
+        
+        color = self.color[indobs]
+        edgecolor = self.edgecolor[indobs]
+        marker = self.marker[indobs]
+        size = self.size[indobs]
+        
+        if color == 'NA':
+            color = default_color
+        if edgecolor == 'NA':
+            edgecolor = default_edge
+        if size == 'NA':
+            size = default_size
+        else:
+            size = float(size)
+        if marker == 'NA':
+            marker = default_marker
+    
+        return color, edgecolor, marker, size
+
+
+    def _plot_fit(self, modif_data: dict, best_model: dict, figsize=(10, 7), uncert: str='yes', trans: str='yes', logx: str='no', logy: str='no', norm: str='no', label_ins: str='no') -> tuple[matplotlib.figure.Figure, matplotlib.axes.Axes, matplotlib.axes.Axes, matplotlib.axes.Axes]:
         '''
         Method to plot the best fit compared with the data, including residuals and filter transmissions.
     
-        Parameters
-        ----------
-        modif_data            (dict): Modified data {indobs: {'spectro': dict, 'photo': dict}}
-        best_model            (dict): Best model {indobs: {'spectro': dict, 'photo': dict}}
-        figsize              (tuple): Figure size
-        uncert                 (str): Plot uncertainties if 'yes'
-        trans                  (str): Plot transmission filters if 'yes'
-        logx                   (str): Use logarithmic x-axis if 'yes'
-        logy                   (str): Use logarithmic y-axis if 'yes'
-        norm                   (str): Normalize spectra if 'yes'
-        label_ins              (str): Show instrument labels if 'yes'
+        Parameters:
+        modif_data (dict): Modified data {indobs: {'spectro': dict, 'photo': dict}}.
+        best_model (dict): Best model {indobs: {'spectro': dict, 'photo': dict}}.
+        figsize   (tuple): Figure size.
+        uncert      (str): Plot uncertainties if 'yes'.
+        trans       (str): Plot transmission filters if 'yes'.
+        logx        (str): Use logarithmic x-axis if 'yes'.
+        logy        (str): Use logarithmic y-axis if 'yes'.
+        norm        (str): Normalize spectra if 'yes'.
+        label_ins   (str): Show instrument labels if 'yes'.
     
-        Returns
-        -------
-        fig     (matplotlib.figure.Figure): Figure object
-        ax      (matplotlib.axes._axes.Axes): Axes object for main spectra
-        axr     (matplotlib.axes._axes.Axes): Axes object for residuals
-        axr2    (matplotlib.axes._axes.Axes): Axes object for residuals histogram
+        Returns:
+        tuple: (fig, ax, axr, axr2) where:
+            - fig is the figure object,
+            - ax is the main axes for spectra,
+            - axr is the axes for residuals,
+            - axr2 is the axes for residuals histogram.
+            
+        Authors: Paulina Palma-Bifani, Matthieu Ravet and Allan Denis
         '''
-    
-        self._logger.info('ForMoSA - Best fit and residuals plot')
         
+        self._logger.info('ForMoSA - Best fit and residuals plot')
+    
+        filter_ax = 'no'
+    
         if not modif_data or not best_model:
             msg = 'Results are empty. Please run the sampling or compute the best model first.'
             self._logger.error(msg)
@@ -312,9 +435,7 @@ class NestedSampling_Plotting(object):
         axr = plt.subplot2grid((9, 11), (6, 0), rowspan=1, colspan=10, sharex=ax)
         axr2 = plt.subplot2grid((9, 11), (6, 10), rowspan=1, colspan=1)
     
-        filter_ax = 'no'
-    
-        # === First pass: collect all residuals globally ===
+        # First pass: collect all residuals globally
         global_residuals = []
         global_flux = []
         for obs, mod in zip(modif_data.values(), best_model.values()):
@@ -342,151 +463,77 @@ class NestedSampling_Plotting(object):
         std_global = np.nanstd(global_residuals)
         if std_global == 0 or np.isnan(std_global):
             std_global = 1.0  # Avoid division by zero
-            
     
         used_labels = set()
     
-        # === Second pass: plot data ===
+        # Second pass: plot data (spectro and photo)
         for indobs, (obs, mod) in enumerate(zip(modif_data.values(), best_model.values())):
             obs_spectro, obs_photo = obs['spectro'], obs['photo']
             mod_spectro, mod_photo = mod['spectro'], mod['photo']
     
             ck_spectro = mod_spectro.get('ck', 1) if norm == 'yes' else 1
             ck_photo = mod_photo.get('ck', 1) if norm == 'yes' else 1
-            
+    
+            # Get plot style for each observation
+            color, edgecolor, marker, size = self._get_plot_style(indobs)
+    
             # --- Spectroscopic data ---
             if len(obs_spectro['wav']) > 0:
                 ins = obs_spectro.get('ins', ['unknown'])[0]
-                color = self.color[indobs]
-                edgecolor = self.edgecolor[indobs]
-                marker = self.marker[indobs]
-                size = self.size[indobs]
-                
-                if color == 'NA':
-                    color = 'magenta'
-                if edgecolor == 'NA':
-                    edgecolor = 'darkmagenta'
-                if size == 'NA':
-                    size = 50
-                else:
-                    size = float(size)
-    
                 obs_wav = np.array(obs_spectro['wav'])
                 obs_flx = np.array(obs_spectro['flx']) / ck_spectro / (10**factor)
                 mod_flx = np.array(mod_spectro['flx']) / ck_spectro / (10**factor)
-                
-                if label_ins == 'no':
-                    label = 'Spectroscopic data' if 'Spectroscopic data' not in used_labels else None
-                else:
-                    label = f'{ins}'
-                if label:
-                    used_labels.add(label)
-
-                err = obs_spectro.get('err', None)
-                if uncert == 'yes' and err is not None and len(err) == len(obs_wav):
-                    ax.errorbar(obs_wav, obs_flx, yerr=np.array(err) / ck_spectro / (10**factor), fmt='none', ecolor=edgecolor, alpha=0.8)
+                err = np.array(obs_spectro.get('err', [])) / ck_spectro / (10**factor) if uncert == 'yes' else None
     
-                if marker == 'NA':
-                    ax.plot(obs_wav, obs_flx, c=color, label=label)
-                    axr.plot(obs_wav, (obs_flx - mod_flx) / std_global, c=color, alpha=0.7)
-                else:
-                    ax.scatter(obs_wav, obs_flx, c=color, edgecolors=edgecolor, marker=marker, s=size, label=label, linewidths=2)
-                    axr.scatter(obs_wav, (obs_flx - mod_flx) / std_global, c=color, edgecolors=edgecolor, marker=marker, s=size, alpha=0.7, linewidths=2)
-                
-                axr.axhline(0, color='k', linestyle='--', alpha=0.5)
+                label = self._get_label(ins, label_ins, used_labels, 'Spectroscopic data')
+                self._plot_data_point(ax, axr, obs_wav, obs_flx, mod_flx, std_global, color, edgecolor, marker, size, label, err)
+    
+                # Residuals histogram
                 axr2.hist((obs_flx - mod_flx) / std_global, bins=100, orientation='horizontal', color='black', alpha=0.8, density=True)
-    
-                ax.plot(obs_wav, mod_flx, c='black')
     
             # --- Photometric data ---
             if len(obs_photo['wav']) > 0:
                 ins = obs_photo.get('ins', ['unknown'])[0]
-                color = self.color[indobs]
-                edgecolor = self.edgecolor[indobs]
-                marker = self.marker[indobs]
-                size = self.size[indobs]
-                
-                if label_ins == 'no':
-                    label = 'Photometric data' if 'Photometric data' not in used_labels else None
-                else:
-                    label = f'{ins}'
-                if label:
-                    used_labels.add(label)
-
-                if color == 'NA':
-                    color = 'blue'
-                if edgecolor == 'NA':
-                    edgecolor = 'darkblue'
-                if size == 'NA':
-                    size = 50
-                else:
-                    size = float(size)
-                if marker == 'NA':
-                    marker = 'o'
-                    
+                obs_wav = np.array(obs_photo['wav'])[0]
+                obs_flx = np.array(obs_photo['flx'])[0] / ck_photo / (10**factor)
+                mod_flx = np.array(mod_photo['flx'])[0] / ck_photo / (10**factor)
+                err = np.array(obs_photo.get('err', [])) / ck_photo / (10**factor) if uncert == 'yes' else None
+    
                 try:
-                    filter_path = u.find_filter_file(ins)
-                    filt = np.load(filter_path)
+                    filt = np.load(u.find_filter_file(ins))
                     x = filt['x_filt']
                     y = filt['y_filt']
                     files_loaded = True
                 except Exception as e:
                     self._logger.warning(f"Could not load filter {ins}: {e}")
                     files_loaded = False
-                
-                obs_wav = np.array(obs_photo['wav'])[0]
-                obs_flx = np.array(obs_photo['flx'])[0] / ck_photo / (10**factor)
-                mod_flx = np.array(mod_photo['flx'])[0] / ck_photo / (10**factor)
-                ax.scatter(obs_wav, obs_flx, color=color, edgecolors=edgecolor, marker=marker, s=size, label=label, linewidths=2)
-                ax.errorbar(obs_wav, obs_flx, xerr=np.array([abs(x[0]-obs_wav),abs(x[-1]-obs_wav)])[:,np.newaxis], ecolor=edgecolor)
-                axr.scatter(obs_wav, (obs_flx - mod_flx) / std_global, c=color, edgecolors=edgecolor, marker=marker, s=size, alpha=0.7, linewidths=2)
-
-                err = obs_photo.get('err', None)
-                if uncert == 'yes' and err is not None:
-                    ax.errorbar(obs_wav, obs_flx, yerr=np.array(err) / ck_photo / (10**factor), fmt='none', ecolor=edgecolor, alpha=0.8)
+                    
+                label = self._get_label(ins, label_ins, used_labels, 'Photometric data')
+                self._plot_data_point(ax, axr, obs_wav, obs_flx, mod_flx, std_global, color, edgecolor, marker, size, label, yerr = err, xerr = np.array([abs(x[0]-obs_wav), abs(x[-1]-obs_wav)])[:,np.newaxis], plot_model='scatter')
     
-                ax.scatter(obs_wav, mod_flx, marker='o', c='black', s=20)
-    
+                # Transmission filters
                 # Create the filter subplot once, outside the loop over instruments
                 if trans == 'yes' and filter_ax == 'no' and files_loaded == True:
-                    axfilt = plt.subplot2grid((9, 11), (0, 0), rowspan=2, colspan=10, sharex=ax)
                     filter_ax = 'yes'
+                    axfilt = plt.subplot2grid((9, 11), (0, 0), rowspan=2, colspan=10, sharex=ax)
                     axfilt.set_ylabel('Transmission')
                     axfilt.tick_params(bottom=False, labelbottom=False)
                     axfilt.set_ylim(0, 1.2)
-                    # Optional: set log scale if needed
                     if logx == 'yes':
                         axfilt.set_xscale('log')
-                
+                        
                 # Then, in the same block (or inside the photo loop), loop over all instruments and plot their filters
                 if trans == 'yes' and files_loaded == True:
                     axfilt.plot(x, y, alpha=0.6, c=color)
-
-    
-        # === Axes formatting ===
-        if len(self.color) > 0:
-            ax.legend(frameon=False)
-    
-        if logx == 'yes':
-            ax.set_xscale('log')
-            axr.set_xscale('log')
-            if trans == 'yes':
-                axfilt.set_xscale('log')
-        if logy == 'yes':
-            ax.set_yscale('log')
-    
-        axr.set_xlabel(r'Wavelength ($\mu$m)')
+                        
+        ax.legend(frameon=False)
+        
         ax.set_ylabel(rf'Flux ($10^{{{factor}}}$ W m$^{-2}$ $\mu$m$^{-1}$)')
+        axr.set_xlabel(r'Wavelength ($\mu$m)')
         axr.set_ylabel(r'Residuals ($\sigma$)')
-    
-        if trans == 'yes':
-            axfilt.set_ylabel('Transmission')
-            axfilt.tick_params(bottom=False, labelbottom=False)
-            axfilt.set_ylim(0, 1.2)
-    
         axr2.axis('off')
         ax.tick_params(bottom=False, labelbottom=False)
     
         return fig, ax, axr, axr2
     
-    
+        
