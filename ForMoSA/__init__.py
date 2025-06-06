da2fc3bb007cd32eb3f0b829248adf96f099ff80
Arthur Vigan <arthur.vigan@lam.fr>
2025-06-06 11:17:18 +0200

    Add high-level utility functions

--- ForMoSA/__init__.py
+++ ForMoSA/__init__.py
@@ -4,3 +4,6 @@ __version__ = "1.1.5"
 from .observation import Observation
 from .analysis import Analysis
 from .global_params import GlobalParams
+
+# high-level utility functions
+from .phototeque import add_filter, list_filters
