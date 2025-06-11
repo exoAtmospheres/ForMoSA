26560e6866d54ba2f06fede498728f2359844712
adenis <adenis@lam.fr>
2025-06-11 10:22:49 +0200

    fixing issue in len(star_flx)

--- ForMoSA/model_grid.py
+++ ForMoSA/model_grid.py
@@ -231,7 +231,7 @@ class ModelGrid(object):
             # Determine whether to remove the continuum
             if res_cont[indobs % len(res_cont)] != 'NA':
                 # High-contrast mode, we do not remove the continuum to the models
-                if len(obs_data[indobs]['spectro']['star_flx'][0]) > 0:
+                if len(obs_data[indobs]['spectro']['star_flx']) > 0:
                     remove_continuum = False
                 # Non high_contrast mode, we remove the continuum to the models
                 else:
