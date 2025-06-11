b21a3aa4d9c91b4f0f07e1e76d3525f80eab63d7
adenis <adenis@lam.fr>
2025-06-11 10:12:08 +0200

    Fixing issue when there is multiple observations in a single fits file

--- ForMoSA/global_params.py
+++ ForMoSA/global_params.py
@@ -101,7 +101,7 @@ class GlobalParameters(object):
                                 # If obs contains only one instrument, we don't do anything
                                 # but if obs contains at least 2 instruments, we repeat
                                 for nb_ins in range(nb_instrument_per_obs - 1):
-                                    val.insert(index, val[index])
+                                    val.insert(index, val[index+1])
                                     index += 1
                 elif cast == list:
                     val = [val]
