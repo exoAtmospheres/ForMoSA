4ee409e0d269431a9fedf7de1dc3cd21425dd53d
adenis <adenis@lam.fr>
2026-01-08 14:59:14 +0100

    Update __init__ for the imports

--- ForMoSA/__init__.py
+++ ForMoSA/__init__.py
@@ -6,6 +6,10 @@ from .analysis import Analysis
 from .global_params import GlobalParameters, GlobalParams
 from .nested_sampling.sampling import NestedSampling
 from .paths import ForMoSAPaths
+from .ForMoSA_logging import setup_logging
+from .Filter import Filter
+from .ForMoSA_enums import WavelengthUnit, FluxUnit, DataUnit, FilterType
 
 # high-level utility functions
 from .phototeque import add_filter, list_filters
+
